1512610427471edaa9a48c324b0b89eb3cef1604
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-06-14 03:09:24 +0600

    update user role

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5000 || process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqpiudt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,22 @@ async function run() {
         app.get("/users", async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
+        });
+
+        app.patch("/users/:id", async (req, res) => {
+            const id = req.params?.id;
+            const userRoleObj = req.body;
+            const filter = { _id: new ObjectId(id) };
+            if (id) {
+                const updateDoc = {
+                    $set: {
+                        role: userRoleObj.userRole,
+                    },
+                };
+
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
         });
 
         // Send a ping to confirm a successful connection
